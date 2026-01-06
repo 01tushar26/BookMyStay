f01dec3e3ee68e58d463fb024a513c274cd18c93
Tushar Tyagi <tushartyagi2601@gmail.com>
2026-01-06 17:48:13 +0530

    first commit

---
+++ hotelManagementsystem-AIRBNB/src/test/java/com/project/tushartyagi/hotelManagementsystem/AIRBNB/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tushartyagi.hotelManagementsystem.AIRBNB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
