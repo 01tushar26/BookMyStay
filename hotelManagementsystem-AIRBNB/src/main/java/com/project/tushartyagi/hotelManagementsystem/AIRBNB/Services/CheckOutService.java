d34ebf8fbcc1ee1c6ab683d88d01b44e994e926c
Tushar Tyagi <tushartyagi2601@gmail.com>
2026-01-24 14:52:48 +0530

    24thjan

--- hotelManagementsystem-AIRBNB/src/main/java/com/project/tushartyagi/hotelManagementsystem/AIRBNB/Services/CheckOutService.java
+++ hotelManagementsystem-AIRBNB/src/main/java/com/project/tushartyagi/hotelManagementsystem/AIRBNB/Services/CheckOutService.java
@@ -3,5 +3,5 @@ package com.project.tushartyagi.hotelManagementsystem.AIRBNB.Services;
 import com.project.tushartyagi.hotelManagementsystem.AIRBNB.Entity.Booking;
 
 public interface CheckOutService {
-    String getCheckoutSession(Booking booking, String sucessUrl, String failureUrl);
+
 }
