e2513a67122ce784368adeb61875001cbed78f70
Tushar Tyagi <tushartyagi2601@gmail.com>
2026-05-24 00:03:51 +0530

    docs : add url, description and tags in swagger config

--- hotelManagementsystem-AIRBNB/src/main/java/com/project/tushartyagi/hotelManagementsystem/AIRBNB/Configurations/Swaggerconfig.java
+++ hotelManagementsystem-AIRBNB/src/main/java/com/project/tushartyagi/hotelManagementsystem/AIRBNB/Configurations/Swaggerconfig.java
@@ -2,7 +2,11 @@ package com.project.tushartyagi.hotelManagementsystem.AIRBNB.Configurations;
 
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +18,28 @@ public class Swaggerconfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("BookMyStay")
+                                .version("1.0")
+                                .description(
+                                        "Production-ready backend APIs for a hotel booking platform. " +
+                                                "Supports JWT authentication, hotel management, room inventory, bookings, " +
+                                                "payments, reporting, and user profile management."
+                                )
+                                .contact(
+                                        new Contact()
+                                                .name("Tushar Tyagi")
+                                                .email("tushartyagi2601@gmail.com")
+                                                .url("https://tushardev.me")
+                                )
+
+                )
+                .externalDocs(
+                        new ExternalDocumentation()
+                                .description("GitHub Repository")
+                                .url("https://github.com/01tushar26/BookMyStay")
+                )
 
 
                 .components(new Components()
